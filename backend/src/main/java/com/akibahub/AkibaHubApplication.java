b39536cb4a2dc55bc05cbde467027a2562000afa
systems-jackal <systemsjackal@gmail.com>
2026-06-14 13:45:24 -0400

    Update the appliction confige for the spring frme work

---
+++ backend/src/main/java/com/akibahub/AkibaHubApplication.java
@@ -0,0 +1,12 @@
+package com.akibahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkibaHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkibaHubApplication.class, args);
+    }
+}
