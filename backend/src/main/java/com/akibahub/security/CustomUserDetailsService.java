2d86810ca9815d5695f27526c6216d4dc7598628
systems-jackal <systemsjackal@gmail.com>
2026-06-17 09:55:01 -0400

    Updated a custome user securiy service

---
+++ backend/src/main/java/com/akibahub/security/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.akibahub.security;
+
+import com.akibahub.model.User;
+import com.akibahub.repository.UserRepository;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
